9cab623633f3cc3cf6fc309e73947da32c648ae7
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2022-06-27 11:08:36 +0800

    change back

--- Sources/LaunchPadManagerDBHelper/LaunchPadManagerDBHelper.swift
+++ Sources/LaunchPadManagerDBHelper/LaunchPadManagerDBHelper.swift
@@ -29,36 +29,16 @@ public struct LaunchPadManagerDBHelper {
         return output
     }
 
-    func getAllAppInfosFromApplication() throws -> [AppInfo] {
-        let content = try FileManager.default.contentsOfDirectory(atPath: "/Applications")
-        return content.filter{
-            ($0 as NSString).pathExtension == "app"
-        }.map { url in
-            let url = URL(universalFilePath: "/Applications/\(url)")
-            return AppInfo(url: url, name: url.deletingPathExtension().lastPathComponent)
-        }
-    }
-
     public func getAllAppInfos() throws -> [AppInfo] {
         let apps = try db.prepare(appsTable).map { row in
             Apps(id: row[Expression<Int>("item_id")], title: row[Expression<String>("title")], bookmark: row[Expression<Data>("bookmark")])
         }
 
-        var infos: [AppInfo] = try apps.compactMap { app in
+        return try apps.compactMap { app in
             var isStale = false
             let url = try URL(resolvingBookmarkData: app.bookmark, bookmarkDataIsStale: &isStale)
             return .init(url: url, name: app.title)
         }
-
-        for app in try getAllAppInfosFromApplication() {
-            if !infos.contains(where: { info in
-                info.url.universalPath() == app.url.universalPath() + "/" || info.name == app.name
-            }) {
-                infos.append(app)
-            }
-        }
-
-        return infos
     }
 
     struct Apps {
